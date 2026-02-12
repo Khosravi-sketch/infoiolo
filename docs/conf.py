7cce33ba7a0b13cbc223e6ef8513d0dba6301ed2
Khosravi-sketch <claraeaton@hotmail.com>
2026-02-12 12:28:37 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,23 +1,11 @@
-# Configuration file for the Sphinx documentation builder
+project = 'My Documentation'
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-
-project = 'Info IOLO'
-copyright = '2025'
-author = 'Skolkin Smith'
-
-release = '1.0'
-
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
-]
+extensions = []
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+html_use_index = True
